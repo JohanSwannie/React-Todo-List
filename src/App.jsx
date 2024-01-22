562313792c3201bec739ec8eccf1b5d77ab2da82
Johan Swan <jswannieman@gmail.com>
2024-01-22 16:34:58 +1300

    Commit App JSX Component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-white text-center bg-blue-950 ">APPLICATION</h1>
+    </>
+  );
+}
+
+export default App;
